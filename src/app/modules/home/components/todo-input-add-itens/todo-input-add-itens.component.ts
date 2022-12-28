6c171c0a0834560041e3573e6c53e9e489f16b41
CristianCMP <cristian.puhl@universo.univates.br>
2022-12-27 21:31:52 -0300

    Finalização do To Do

---
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input-add-itens',
+  templateUrl: './todo-input-add-itens.component.html',
+  styleUrls: ['./todo-input-add-itens.component.scss']
+})
+export class TodoInputAddItensComponent implements OnInit {
+
+  @Output() public emitItemTaskList = new EventEmitter();
+
+  public addItemTaskList: string = "";
+
+  constructor() { }
+
+  public submitItemTaskList() {
+
+    this.addItemTaskList = this.addItemTaskList.trim();
+
+    if (this.addItemTaskList) {
+      console.log(this.addItemTaskList);
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
